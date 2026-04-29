4b1ced36545415463994daa2b9cbf5405242aeda
Basimsalman <basimxsalman@gmail.com>
2026-04-29 15:57:21 -0700

    fix login url

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
 
   const handleLogin = async () => {
     try {
-      await axios.post("https://mental-health-backend-h88t.onrender.com", {
+      await axios.post("https://mental-health-backend-h88t.onrender.com/login/", {
         username,
         password,
       });
